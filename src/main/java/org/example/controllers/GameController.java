91766c558c95f852fdd6aabfdf2ae74f44e04cae
Rudraksh Rankawat <rudrakshrankawat@Rudrakshs-Mac.local>
2024-04-24 17:28:03 +0530

    Initial commit: uploading all files

---
+++ src/main/java/org/example/controllers/GameController.java
@@ -0,0 +1,34 @@
+package org.example.controllers;
+
+import org.example.exceptions.InvalidMoveException;
+import org.example.models.Game;
+import org.example.models.GameState;
+import org.example.models.Player;
+
+import java.util.List;
+
+public class GameController {
+    public Game startGame(int dimension, List<Player> players) {
+
+        //if 2 player have same symbol then throw exception
+
+        return new Game(dimension, players);
+    }
+
+    public void makeMove(Game game) throws InvalidMoveException {
+        game.makeMove();
+    }
+
+    public GameState checkState(Game game) {
+        return game.getGameState();
+    }
+
+    public Player getWinner(Game game) {
+        return game.getWinner();
+    }
+
+    public void printBoard(Game game) {
+        game.printBoard();
+    }
+
+}
---
+++ src/main/java/org/example/models/Board.java
@@ -0,0 +1,53 @@
+package org.example.models;
+
+import java.util.*;
+
+public class Board {
+    private int size;
+    private List<List<Cell>> board;
+
+    public Board(int size) {
+        this.size = size;
+        board = new ArrayList<>();
+
+        for(int i = 0; i < size; i++) {
+            List<Cell> row = new ArrayList<>();
+            for(int j = 0; j < size; j++) {
+               row.add(new Cell(i, j));
+            }
+            board.add(row);
+        }
+
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public List<List<Cell>> getBoard() {
+        return board;
+    }
+
+    public void setBoard(List<List<Cell>> board) {
+        this.board = board;
+    }
+
+    public void printBoard() {
+        for (List<Cell> row : board) {
+            for (Cell cell : row) {
+                if (cell.getCellState().equals(CellState.EMPTY)) {
+                    System.out.print("| -- |");
+                } else {
+                    System.out.print("| " + cell.getPlayer().getSymbol().getaChar() + " |");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+}
